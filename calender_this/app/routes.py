379acfb0acb2d30e160c86ce689e64a0fa786172
agabriele73 <agabrielejr@gmail.com>
2023-05-03 21:52:04 -0700

    some stuff done

--- calender_this/app/routes.py
+++ calender_this/app/routes.py
@@ -1,5 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+import os
 
 
-bp = ('blueprint')
+calendar_bp = Blueprint('calendar_bp', __name__, url_prefix='/')
+DB_FILE=os.environ.get('DB_FILE')
 
+
+@calendar_bp.route('/')
+def main():
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        rows = curs.fetchall()
+        return render_template('main.html', rows=rows)
